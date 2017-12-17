dce3e883620c97d9c841b8304a6949c6a1c89dd2
Arshega <arriesgavincent@yahoo.com>
2017-12-17 21:07:01 +0800

    Missing Fragment, and Shuffle Button

--- app/src/main/java/com/example/pc/spotify/CustomAdapter.kt
+++ app/src/main/java/com/example/pc/spotify/CustomAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.pc.spotify
 
+import android.content.ClipData
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -18,11 +20,26 @@ class CustomAdapter(val userList: ArrayList<Songs>, context: Context) : Recycler
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
         val songs : Songs = userList[position]
-        var data =position
+        var row_index = -1
+        var currentItem = null
+
         holder?.textViewName?.text = songs.songName
         holder?.textViewName2?.text = songs.album
         holder!!.textViewName.text =  songs.songName
         holder.textViewName2.text = songs.album
+
+//        holder.linearLayout.setOnClickListener(View.OnClickListener {
+//            row_index = position
+//            notifyDataSetChanged()
+//        })
+//        if (row_index === position) {
+//            holder.linearLayout.setBackgroundColor(Color.parseColor("#567845"))
+//            holder.textViewName.setTextColor(Color.parseColor("#ffffff"))
+//        } else {
+//            holder.linearLayout.setBackgroundColor(Color.parseColor("#ffffff"))
+//            holder.textViewName.setTextColor(Color.parseColor("#000000"))
+//        }
+
     }
 
 
@@ -44,13 +61,13 @@ class CustomAdapter(val userList: ArrayList<Songs>, context: Context) : Recycler
 
         val textViewName: TextView
         val textViewName2: TextView
-        val linearLayout: LinearLayout
+        val linearLayout: ConstraintLayout
         var customItemClick: CustomItemClick?=null
 
         init {
             textViewName = itemView.findViewById<TextView>(R.id.textViewUsername)
             textViewName2 = itemView.findViewById<TextView>(R.id.textViewAddress)
-            linearLayout = itemView.findViewById<LinearLayout>(R.id.LinearLayout)
+            linearLayout = itemView.findViewById<ConstraintLayout>(R.id.LinearLayout)
             itemView.setOnClickListener(this)
         }
 
--- app/src/main/java/com/example/pc/spotify/MainActivity.kt
+++ app/src/main/java/com/example/pc/spotify/MainActivity.kt
@@ -1,10 +1,25 @@
 package com.example.pc.spotify
 
+import android.content.res.Resources
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.ActionBar
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import disableMenuShiftMode
+import android.widget.TextView
+import android.widget.Toolbar
+import kotlinx.android.synthetic.main.action_bar_new.*
+import kotlinx.android.synthetic.main.song_list.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +29,12 @@ class MainActivity : AppCompatActivity() {
 
         val recycleview = findViewById<RecyclerView>(R.id.recyclerView)
 
-        recycleview.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
+
+
+        recycleview.layoutManager = LinearLayoutManager(this, android.widget.LinearLayout.VERTICAL, false)
 
         val songs = ArrayList<Songs>()
+
 
         songs.add(Songs("Come Fly With Me - Remastered", "Frank Sinatra • Ultimate Sinatra: The Centennial Collection"))
         songs.add(Songs("The Way You Look Tonight", "Frank Sinatra • Ultimate Sinatra: The Centennial Collection"))
@@ -29,10 +47,28 @@ class MainActivity : AppCompatActivity() {
         songs.add(Songs("The Longest Time", "Billy Joel • The Essential Billy Joel"))
         songs.add(Songs("Leave A Tender Momemnt Alone", "Billy Joel • An Innocent Man"))
         songs.add(Songs("Should I Stay or Should I Go - Remastered", "The Clash • Combat Rock"))
-        songs.add(Songs("Your Song", "Elton John • Rocket Man (Deluxe Edition)"))
+        songs.add(Songs("Your Song", "Elton John • Ultimate Sinatra: The Centennial Collection"))
 
         val adapter = CustomAdapter(songs, applicationContext)
 
         recycleview.adapter = adapter
+
+        val navView = findViewById<BottomNavigationView>(R.id.navigation) as BottomNavigationView
+        disableMenuShiftMode(navView)
+
+        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar()?.setCustomView(R.layout.action_bar_new);
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar()?.setDisplayShowHomeEnabled(true);
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.threedotmenu,menu)
+        return true
+    }
+
+
+
+
 }
